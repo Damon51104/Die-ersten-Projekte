3e16cf4506d4cd851f1b74c2895cbbd19d2ca23a
Damon51104 <91246132+Damon51104@users.noreply.github.com>
2021-10-05 08:29:48 +0200

    Add files via upload

---
+++ Chatbot ada.py
@@ -0,0 +1,13 @@
+chat = input("Type here: ")
+
+if "hey" in chat:
+    print ("Hey my name is Ada! What is yours?")
+    input()
+
+if "hi" in chat:
+    print ("Hi my name is Ada! What is yours?")
+    input()
+
+if "hello" in chat:
+    print ("Hello my name is Ada! What is yours?")
+    input()
